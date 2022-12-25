2060c6e4c45aba250e310c160f3539f0abe925a3
sohailamahmoud <suhilamahmoud@gmail.com>
2022-12-25 06:25:07 +0200

    Fixing Upload

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+
 dimensions1=[]
 dimensions2=[]
 fourier1= []
@@ -28,33 +29,41 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods= ['GET','POST'])
 def upload_image():
-    if 'files[]' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    files = request.files.getlist('files[]')
-    file_names = []
-    for file in files:
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file_names.append(filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        else:
-            flash('Allowed image types are -> png, jpg, jpeg, gif')
+    if request.method == 'POST' :
+        if 'files[]' not in request.files:
+            flash('No file part')
             return redirect(request.url)
+        files = request.files.getlist('files[]')
+        file_names = []
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file_names.append(filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            else:
+                flash('Allowed image types are -> png, jpg, jpeg, gif')
+                return redirect(request.url)
  
-    img_a , freq_a , magnitude_spectrum_a , phase_spectrum_a = functions.imageFourier(file=os.path.join(app.config['UPLOAD_FOLDER'],file_names[0]))
-    img_b , freq_b , magnitude_spectrum_b , phase_spectrum_b = functions.imageFourier(file=os.path.join(app.config['UPLOAD_FOLDER'],file_names[1]))
+        img_a = functions.readImage(file=os.path.join(app.config['UPLOAD_FOLDER'],file_names[0]))
+        img_b = functions.readImage(file=os.path.join(app.config['UPLOAD_FOLDER'],file_names[1]))
 
-    functions.plotspectrums(img_a , magnitude_spectrum_a , phase_spectrum_a , img_b , magnitude_spectrum_b , phase_spectrum_b)
+        freq_a , magnitude_spectrum_a , phase_spectrum_a = functions.imageFourier(img=img_a)
+        freq_b , magnitude_spectrum_b , phase_spectrum_b = functions.imageFourier(img=img_b)
 
-    
+        functions.plotspectrums(img_a , magnitude_spectrum_a , phase_spectrum_a , img_b , magnitude_spectrum_b , phase_spectrum_b)
 
-    combined = np.multiply(np.abs(freq_a), np.exp(1j*np.angle(freq_b)))
-    imgCombined = np.real(np.fft.ifft2(combined))
+        if np.size(img_b) != np.size(img_a) :
+            new_height, new_width = np.shape(img_a)
+            img_b = cv2.resize(img_b, dsize=[new_width,new_height])
+            freq_b = np.fft.fft2(img_b)
 
-    plt.imsave('static\Images\output.png',imgCombined, cmap='gray')
+
+        combined = np.multiply(np.abs(freq_a), np.exp(1j*np.angle(freq_b)))
+        imgCombined = np.real(np.fft.ifft2(combined))
+
+        plt.imsave('static\Images\output.png',imgCombined, cmap='gray')
 
 
     return render_template('Mixture.html')
@@ -76,7 +85,7 @@ def test():
     indx4= list(result.keys())[3]
     functions.cut('static/Images/sky.jpg',result.get(indx1), result.get(indx2), result.get(indx3), result.get(indx4))
     functions.cut('static/Images/train.jpg',dimensions1.get(indx1), dimensions1.get(indx2), dimensions1.get(indx3), dimensions1.get(indx4),1)
-    return result
+    return render_template("Mixture.html",result)
 
 
 @app.route('/test2', methods=['POST'])
@@ -94,11 +103,60 @@ def test2():
     indx4= list(result.keys())[3]
     #imagename= 
     functions.cut('static/Images/train.jpg',dimensions2.get(indx1), dimensions2.get(indx2), dimensions2.get(indx3), dimensions2.get(indx4),2)
-    return result
+    return render_template("Mixture.html",result)
 
 
 
+@app.route('/imageMixing')
+def combination_cropped(option1, option2):
+    img1 , fourier1 , mag1 , phase1 = functions.imageFourier("cropped1.jpg")
+    img2 , fourier2 , mag2 , phase2 = functions.imageFourier("cropped2.jpg")
+    flat1= list(img1.flatten())
+    flat2= list(img2.flatten())
+    modified= flat2
+    maxrows= img1.shape[0]
+    maxcolumns= img1.shape[1]
+    maxrows2= img2.shape[0]
+    maxcolumns2= img2.shape[1]
+    count=1
+    diff= np.abs(maxcolumns-maxcolumns2)
+    if len(flat1)  > len(flat2):  
+        #print(img2)
+        #print(img2.shape)
+        #print("--------------------------------------------------------------------------------------")
+        #print(flat2)   
+        #print("--------------------------------------------------------------------------------------")
+        for i in np.arange(maxcolumns2,maxcolumns2*maxrows2,maxcolumns2+diff):
+            for j in np.arange(i,(maxcolumns*count)):
+                flat2.insert(j, 0) 
+            count+=1
+        while(len(flat1) > len(flat2)):
+            flat2.append(0)
+        modified= np.array(flat2).reshape(img1.shape)
+        #print(modified)
+        fourier2 = np.fft.fft2(modified)
+    if len(flat1)  < len(flat2):
+        for i in np.arange(maxcolumns,maxcolumns*maxrows,maxcolumns+diff):
+            for j in np.arange(i,(maxcolumns2*count)):
+                flat1.insert(j, 0) 
+            count+=1
+        while(len(flat2) > len(flat1)):
+            flat1.append(0)
+        modified= np.array(flat1).reshape(img2.shape)
+        fourier1 = np.fft.fft2(modified)
 
+    if option1== True:              # first Phase and second mag
+        combined = np.multiply(np.abs(fourier2), np.exp(1j*np.angle(fourier1)))
+        imgCombined = np.real(np.fft.ifft2(combined))
+        plt.imsave('Images\output_cropped.jpg',imgCombined, cmap='gray')
+
+    elif option2== True:                    # first mag and second phase
+        combined = np.multiply(np.abs(fourier1), np.exp(1j*np.angle(fourier2)))
+        imgCombined = np.real(np.fft.ifft2(combined))
+        plt.imsave('Images\output_cropped.jpg',imgCombined, cmap='gray')
+
+
+    return render_template("Mixture.html")
 
 
 
--- functions.py
+++ functions.py
@@ -7,14 +7,19 @@ from matplotlib.ticker import PercentFormatter
 from PIL import Image
 
 
-def imageFourier(file):
-    img = cv2.imread(file, 0)
+
+def readImage(file):
+    img = cv2.imread(file,0)
+    return img
+
+
+def imageFourier(img):
     f = np.fft.fft2(img)
     fshift = np.fft.fftshift(f)
     magnitude_spectrum = 20*np.log(np.abs(fshift))
     phase_spectrum = np.angle(fshift)
 
-    return img , f , magnitude_spectrum , phase_spectrum
+    return f , magnitude_spectrum , phase_spectrum
 
 
 def cut(path,x,y,w,h,image):
@@ -28,18 +33,10 @@ def cut(path,x,y,w,h,image):
 
 
 def plotspectrums(img_a , magnitude_spectrum_a , phase_spectrum_a ,img_b , magnitude_spectrum_b , phase_spectrum_b):
-    # plt.subplot(121),plt.imshow(img, cmap = 'gray')
-    # plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-    # plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-    # plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-    # plt.subplot(123),plt.imshow(phase_spectrum, cmap = 'gray')
-    # plt.title('Phase Spectrum'), plt.xticks([]), plt.yticks([])
-    # plt.show()
-    # fig, (ax1, ax2,ax3) = plt.subplots(1, 3)
-    # fig.suptitle('Image')
-    plt.imsave( 'D:\Edu\DSP\Tasks\Task4\DSP_Task_4\static\Images\image1.png',img_a ,cmap = 'gray' )
-    plt.imsave('D:\Edu\DSP\Tasks\Task4\DSP_Task_4\static\Images\magnitude1.png' ,magnitude_spectrum_a ,cmap = 'gray' )
-    plt.imsave('D:\Edu\DSP\Tasks\Task4\DSP_Task_4\static\Images\phase1.png',phase_spectrum_a ,cmap = 'gray' )
-    plt.imsave( 'D:\Edu\DSP\Tasks\Task4\DSP_Task_4\static\Images\image2.png',img_b ,cmap = 'gray' )
-    plt.imsave('D:\Edu\DSP\Tasks\Task4\DSP_Task_4\static\Images\magnitude2.png' ,magnitude_spectrum_b ,cmap = 'gray' )
-    plt.imsave('D:\Edu\DSP\Tasks\Task4\DSP_Task_4\static\Images\phase2.png',phase_spectrum_b ,cmap = 'gray' )
+
+    plt.imsave( 'static\Images\image1.png',img_a ,cmap = 'gray' )
+    plt.imsave('static\Images\magnitude1.png' ,magnitude_spectrum_a ,cmap = 'gray' )
+    plt.imsave('static\Images\phase1.png',phase_spectrum_a ,cmap = 'gray' )
+    plt.imsave( 'static\Images\image2.png',img_b ,cmap = 'gray' )
+    plt.imsave('static\Images\magnitude2.png' ,magnitude_spectrum_b ,cmap = 'gray' )
+    plt.imsave('static\Images\phase2.png',phase_spectrum_b ,cmap = 'gray' )
