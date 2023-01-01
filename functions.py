41cece0c175baeda90431fa83f0071d07c35c526
SaraElwatany <93448764+SaraElwatany@users.noreply.github.com>
2023-01-01 02:37:55 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -18,14 +18,13 @@ def imageFourier(img):
     fshift = np.fft.fftshift(f)
     magnitude_spectrum = 20*np.log(np.abs(fshift))
     phase_spectrum = np.angle(fshift)
-
     return f , magnitude_spectrum , phase_spectrum
 
 
 def combined(freq_mag, freq_phase):
     combined = np.multiply(np.abs(freq_mag), np.exp(1j*np.angle(freq_phase)))
     imgCombined = np.real(np.fft.ifft2(combined))
-    plt.imsave('static\Images\output.png',imgCombined, cmap='gray')
+    plt.imsave('static/Images/output.png',imgCombined, cmap='gray')
 
 
     
@@ -35,20 +34,38 @@ def mask(image,imageno,type,x1,y1,x2,y2):
     x= np.zeros(shape, dtype='uint8')
     x= cv2.rectangle(x, (x1,y1), (x2,y2),(255,255,255),-1)
     imgcropped= cv2.bitwise_and(img,img,mask=x)
-    imgcropped.save(f'static/Images/{imageno}{image}')
+    cv2.imwrite(f'static/Images/masked{type}{imageno}.png', imgcropped)
+    #plt.imsave(f'static/Images/masked{type}{imageno}.png',imgcropped ,cmap = 'gray' )
+    
+
+def and_mask(image1,image2,type,no):
+    img1= readImage(image1)
+    img2= readImage(image2)
+    anded_img= cv2.bitwise_and(img1,img2)
+    cv2.imwrite(f'static/Images/{type}{no}.png', anded_img)
+    #plt.imsave(f'static\Images\masked{type}{imageno}.png',imgcropped ,cmap = 'gray' )
+
+
+def or_mask(image1,image2,type,no):
+    img1= readImage(image1)
+    img2= readImage(image2)
+    ored_img= cv2.bitwise_or(img1,img2)
+    cv2.imwrite(f'static/Images/{type}{no}.png', ored_img)
     
 
 
-def uniform_mask(image,imageno,type,x1,y1,x2,y2):
-    img = cv2.imread(image, 0)
+
+def uniform_mask(image,imageno,type):
+    img = readImage(image)
     shape= np.shape(img)
-    x= np.ones(shape, dtype='uint8')
-    x.save(f'static/Images/uniform{image}')
+    new_img = np.ones(shape, dtype = np.uint8)
+    uni_img = 255*new_img
+    cv2.imwrite(f'static/Images/uniform{type}{imageno}.png', uni_img)
+    #uni_img.save(f'static/Images/uniform{type}{imageno}.png')
+    
 
     
     
 def plotspectrums(magnitude_spectrum_a , phase_spectrum_a ,number):  
-    plt.imsave(f'static\Images\magnitude{number}.png' ,magnitude_spectrum_a ,cmap = 'gray' )
-    plt.imsave(f'static\Images\phase{number}.png',phase_spectrum_a ,cmap = 'gray' )
-    # plt.imsave( 'static\Images\image1.png',img_a ,cmap = 'gray' )
-    # plt.imsave( 'static\Images\image2.png',img_b ,cmap = 'gray' )
+    plt.imsave(f'static/Images/magnitude{number}.png' ,magnitude_spectrum_a ,cmap = 'gray' )
+    plt.imsave(f'static/Images/phase{number}.png',phase_spectrum_a ,cmap = 'gray' )
